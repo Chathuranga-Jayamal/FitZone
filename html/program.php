9f9dfef6ac258555aa8920a787eaf6c9a755afae
Chathuranga-Jayamal <chathurangakpj@gmail.com>
2025-03-22 13:33:28 +0530

    complete the program overview section

--- html/program.php
+++ html/program.php
@@ -6,21 +6,35 @@
     <title>Document</title>
     <link rel="stylesheet" href="/FitZone/CSS/program.css?v=1.0">
 </head>
-<body background-color="black">
+<body>
     <section id="introduction">
-        <?php include 'header.php' ?>
+        <?php include 'header.php'; ?>
         <div class="introduction-container">
-            
-        <h1>Your Fitness, Your Way</h1>
-        <h3>👇 Find the RIGHT Program.</h3>
-
+            <h1>Your Fitness, Your Way</h1>
+            <h3>👇 Find the RIGHT Program.</h3>
         </div>
-    </section>
-    <section id="programs-statistics">
-        <div class="data-cantainer">
-           
+    </section id="overview">
+    <section id="programs-overview">
+        <div class="program-overview-container">
+           <div class="program-overview-card card1">
+            <img height="70" width="70"  src="../images/logos/weightlifting.png" alt="">
+            <h2 style="font-weight: bold;">Choose Your Program</h2>
+            <p>Choose between group sessions for motivation or one-on-one coaching for a personalized fitness journey.</p>
+            <button type="button" class="btn btn-primary"> 🡻 See More</button>
+           </div>
+           <div class="program-overview-card card2">
+           <img height="70" width="70" src="../images/logos/coach.png" alt="">
+            <h2 style="font-weight: bold;">Coaches Who <br> Care</h2>
+            <p>Meet our dedicated coaches who are passionate about helping you reach your fitness goals.</p>
+            <button type="button" class="btn btn-primary"> 🡻 See More</button>
+           </div>
+           <div class="program-overview-card card3">
+            <img height="70" width="70" src="../images/logos/meditation.png" alt="">
+            <h2 style="font-weight: bold;">Level Up With Classes</h2>
+            <p>Explore advanced classes like cardio, strength training, and yoga to boost endurance, flexibility, and power.</p>
+            <button type="button" class="btn btn-primary"> 🡻 See More</button>
+           </div>
         </div>
     </section>
 </body>
-    
 </html>
