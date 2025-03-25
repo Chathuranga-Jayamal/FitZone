f6d23b0cb616e53cb24eb2aedb2cd4f1db0e3541
Chathuranga-Jayamal <chathurangakpj@gmail.com>
2025-03-25 23:52:05 +0530

    Fix some bugs and modify the header and program files

--- html/program.php
+++ html/program.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="/FitZone/CSS/program.css?v=1.0">
+    <title>Programs</title>
+    <link rel="stylesheet" href="/FitZone/CSS/program.css?v=1.1">
 </head>
+
 <body>
     <section id="introduction">
         <?php include 'header.php'; ?>
@@ -13,28 +15,50 @@
             <h1>Your Fitness, Your Way</h1>
             <h3>👇 Find the RIGHT Program.</h3>
         </div>
-    </section id="overview">
+    </section>
     <section id="programs-overview">
         <div class="program-overview-container">
-           <div class="program-overview-card card1">
-            <img height="70" width="70"  src="../images/logos/weightlifting.png" alt="">
-            <h2 style="font-weight: bold;">Choose Your Program</h2>
-            <p>Choose between group sessions for motivation or one-on-one coaching for a personalized fitness journey.</p>
-            <button type="button" class="btn btn-primary"> 🡻 See More</button>
-           </div>
-           <div class="program-overview-card card2">
-           <img height="70" width="70" src="../images/logos/coach.png" alt="">
-            <h2 style="font-weight: bold;">Coaches Who <br> Care</h2>
-            <p>Meet our dedicated coaches who are passionate about helping you reach your fitness goals.</p>
-            <button type="button" class="btn btn-primary"> 🡻 See More</button>
-           </div>
-           <div class="program-overview-card card3">
-            <img height="70" width="70" src="../images/logos/meditation.png" alt="">
-            <h2 style="font-weight: bold;">Level Up With Classes</h2>
-            <p>Explore advanced classes like cardio, strength training, and yoga to boost endurance, flexibility, and power.</p>
-            <button type="button" class="btn btn-primary"> 🡻 See More</button>
-           </div>
+            <div class="program-overview-card card1">
+                <img height="70" width="70" src="../images/logos/weightlifting.png" alt="">
+                <h2 style="font-weight: bold;">Choose Your Program</h2>
+                <p>Choose between group sessions for motivation or one-on-one coaching for a personalized fitness journey.</p>
+                <button type="button" class="btn btn-primary" onclick="location.href='#programs'"> 🡻 See More</button>
+            </div>
+            <div class="program-overview-card card2">
+                <img height="70" width="70" src="../images/logos/coach.png" alt="">
+                <h2 style="font-weight: bold;">Coaches Who <br> Care</h2>
+                <p>Meet our dedicated coaches who are passionate about helping you reach your fitness goals.</p>
+                <button type="button" class="btn btn-primary"> 🡻 See More</button>
+            </div>
+            <div class="program-overview-card card3">
+                <img height="70" width="70" src="../images/logos/meditation.png" alt="">
+                <h2 style="font-weight: bold;">Level Up With Classes</h2>
+                <p>Explore advanced classes like cardio, strength training, and yoga to boost endurance, flexibility, and power.</p>
+                <button type="button" class="btn btn-primary"> 🡻 See More</button>
+            </div>
         </div>
     </section>
+    <section id="programs">
+        <div class="program-container">
+        <div class="program-title">
+            <h1>Our Programs</h1>
+        </div>
+        <!-- <div class="group-program">
+            <img src="../images/photos/group2.jpg" alt="group img">
+            <div class="title">
+                <h1>Group Program</h1>
+            </div>
+        </div>
+        <div class="individual-program">
+            <div class="title">
+                <h1>Indivisual Program</h1>
+            </div>
+
+        </div> -->
+
+        </div>
+
+    </section>
 </body>
+
 </html>
