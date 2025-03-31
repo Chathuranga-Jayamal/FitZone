a6ad80638747ae681ce4675538e134d0f8d9cab4
Chathuranga-Jayamal <chathurangakpj@gmail.com>
2025-03-31 14:31:36 +0530

    Completed personalized webpage

--- html/personalized.php
+++ html/personalized.php
@@ -5,6 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Personalized</title>
+    <!-- Swiper CDN CSS link -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css">
+    <!-- CSS link -->
     <link rel="stylesheet" href="/FitZone/CSS/personalized.css">
 </head>
 
@@ -138,6 +141,72 @@
             </div>
         </div>
     </section>
+    <section id="workouts">
+        <div class="container workouts-container">
+            <div class="workouts-details">
+                <h1 class="fw-bolder workouts-title mb-4">Transform Your Fitness with Our Workouts</h1>
+                <p>Discover a variety of dynamic workout programs designed to keep you motivated and challenged.
+                    Whether you prefer strength training, high-energy cardio, or flexibility-focused routines,
+                    we have something for everyone. Our expert-led workouts ensure that every session is engaging,
+                    effective, and tailored to your fitness journey. Explore our workout plans and find the perfect fit for your lifestyle!</p>
+            </div>
+            <div class=" container swiper mt-5">
+                <div class="workouts-images card-wrapper">
+                    <ul class="workouts-images-list swiper-wrapper p-0">
+                        <li class="swiper-slide workouts-image workouts-1">
+                            <h2>Mastering Dumbbells</h2>
+                            <p>Build strength and endurance with expert-guided dumbbell exercises for all levels.</p>
+                        </li>
+                        <li class="swiper-slide workouts-image workouts-2">
+                            <h2>High-Intensity Cardio</h2>
+                            <p>Enhance your stamina and cardiovascular health with expert-guided treadmill & cardio workouts.</p>
+                        </li>
+                        <li class="swiper-slide workouts-image workouts-3">
+                            <h2>Lat Strength</h2>
+                            <p>Sculpt a powerful back and improve upper-body strength with precise lat pulldown techniques.</p>
+                        </li>
+                        <li class="swiper-slide workouts-image workouts-4">
+                            <h2>Cable Training</h2>
+                            <P>Engage multiple muscle groups with dynamic cable exercises for strength and flexibility.</P>
+                        </li>
+                        <li class="swiper-slide workouts-image workouts-5">
+                            <h2>Strength Mastery</h2>
+                            <p>Develop raw power and perfect your technique in squats, bench press, and deadlifts.</p>
+                        </li>
+                    </ul>
+                    <div class="swiper-pagination"></div>
+
+                    <div class="swiper-button-prev"></div>
+                    <div class="swiper-button-next"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="how">
+        <div class="container how-container">
+            <div class="how-title mt-2 mb-2">
+                <h1 class="fw-bolder">How it Works?</h1>
+            </div>
+            <div class="how-description">
+                <p>During your sessions, your Personal Trainer is fully dedicated to you, ensuring that every workout is 
+                    tailored to your unique fitness level and goals. From the moment you step into the gym, your trainer will assess 
+                    your strengths, weaknesses, and preferences to create a customized plan that keeps you motivated and challenged. 
+                    Whether you're looking to build muscle, lose weight, or improve overall endurance, your trainer will guide you 
+                    through every exercise with expert coaching and real-time feedback.</p>
+                <p>With personalized attention, expert guidance, and a results-driven approach, your Personal Trainer is there to support you 
+                    every step of the way—helping you unlock your full potential and achieve lasting fitness success.</p>
+            </div>
+            <div class="how-buttons">
+                <button type="button" class="btn btn-outline-primary btn-lg me-2" onclick="location.href='./membership.php'" >Get Membership</button>
+                <button type="button" class="btn btn-primary btn-lg" onclick="window.open('https://wa.me/94761894986', '_blank')">Contact us</button>
+            </div>
+        </div>
+    </section>
+    <?php include 'footer.php' ?>
+    <!-- Swiper JS link -->
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <!-- custom js link -->
+     <script src="/FitZone/JS/personalized.js"></script>
 </body>
 
 </html>
