e2cee01a050dbc45b39fb9de2005502d734adc51
Chathuranga-Jayamal <chathurangakpj@gmail.com>
2025-04-02 02:30:48 +0530

    Update the login and signup pages and add mobile responsiveness

--- login/login.php
+++ login/login.php
@@ -1,31 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <!-- Link to css file -->
     <link rel="stylesheet" href="./login.css">
-
 </head>
+
 <body>
-    <!-- Navigation bar & icon -->
-    <?php include '../header/header.php'?>
-    
-    <!-- Login Container -->
-    <div class="body-container">
-    <div class="form-box">
+    <?php include __DIR__ . '/../header/header.php'; ?>
+
+    <div class="container body-container">
+        <div class="login-container">
         <h1>Login</h1>
-        <form action="">
-            <input type="text" placeholder="Username"><br><br>
-            <input type="password" placeholder="Password"><br><br>
+        <form class="login-form" action="" method="POST">
+            <input type="text" name="username" placeholder="Username" required><br><br>
+            <input type="password" name="password" placeholder="Password" required><br><br>
+            <div class="btn-container">
             <button class="submitbtn btn btn-primary" type="submit">Login</button>
             <button class="resetbtn btn btn-outline-primary" type="reset">Cancel</button>
+            </div>
         </form>
-     </div>
+    </div>
     </div>
 
-    <!-- Footer -->
-     <?php include '../footer/footer.php'?>
+    <?php include __DIR__ . '/../footer/footer.php'; ?>
 </body>
+
 </html>
--- singup/singup.php
+++ singup/singup.php
@@ -1,26 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sign Up</title>
+    <title>Login</title>
+    <!-- Link to css file -->
     <link rel="stylesheet" href="./signup.css">
 </head>
+
 <body>
-    <?php include '../header/header.php'?>
-    
-    <div class="body-container">
-    <div class="form-box">
-        <h1>Sing Up</h1>
-        <form action="">
-            <input type="text" placeholder="Email"><br><br>
-            <input type="password" placeholder="Password"><br><br>
-            <button class="submitbtn btn btn-primary" type="submit">Singup</button>
+    <?php include __DIR__ . '/../header/header.php'; ?>
+
+    <div class="container body-container">
+        <div class="signup-container">
+        <h1>Sign Up</h1>
+        <form class="signup-form" action="" method="POST">
+            <input type="email" name="username" placeholder="Email" required><br><br>
+            <input type="password" name="password" placeholder="Password" required><br><br>
+            <div class="btn-container">
+            <button class="submitbtn btn btn-primary" type="submit">Sign Up</button>
             <button class="resetbtn btn btn-outline-primary" type="reset">Cancel</button>
+            </div>
         </form>
-     </div>
+    </div>
     </div>
 
-    <?php include '../footer/footer.php'?>
+    <?php include __DIR__ . '/../footer/footer.php'; ?>
 </body>
+
 </html>
