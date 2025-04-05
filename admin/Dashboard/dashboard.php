c2677ac2075822249e4fbd1df1daaab977af0d8c
Chathuranga-Jayamal <chathurangakpj@gmail.com>
2025-04-05 19:43:39 +0530

    Add Admin Dashboard

---
+++ admin/Dashboard/dashboard.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <!-- Google Icons -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- CSS -->
+    <link rel="stylesheet" href="./dashboard.css">
+    <!-- nav-bar -->
+     <?php include "../../header/header.php"?>
+</head>
+
+<body>
+    <div class="container admin-container">
+        <!-- slide bar -->
+        <aside class="admin-sidebar">
+            <div class="action-buttons" style="margin-top: 10px;">
+                <button class="btn btn-outline-primary" onclick="window.location.href='./dashboard.php';">Dashboard</button>
+                <button class="btn btn-outline-primary" onclick="window.location.href='../Employee/employee.php'">Employee</button>
+                <button class="btn btn-outline-primary">User</button>
+                <button class="btn btn-outline-primary">Trainer</button>
+                <button class="btn btn-outline-primary">Members</button>
+                <button class="btn btn-outline-primary">Classes</button>
+                <button class="btn btn-outline-primary"> Registration</button>
+                <button class="btn btn-outline-primary">Appointments</button>
+                <button class="btn btn-outline-primary"> Queries</button>
+                <button class="btn btn-outline-primary">Promotions</button>
+                <button class="btn btn-outline-primary">Blogs</button>
+            </div>
+        </aside>
+        <!-- view account details -->
+        <main class="main-content content1 active">
+            <h2 class="page-title">Admin Dashboard</h2>
+
+
+        </main>
+    </div>
+
+</body>
+
+</html>
